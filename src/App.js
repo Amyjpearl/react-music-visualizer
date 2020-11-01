b202360a519a56beffa42ff4da9bf2077a0ad7da
AmyJPearl <amyperelberg@gmail.com>
2020-10-31 22:20:16 -0400

    Styles placeholding components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
-import MusicNote from "./assets/music-note.png"
+import MusicNote from "./assets/music-note.png";
+import VisualizerPlaceholder from "./assets/visualizer-placeholder.png";
+import TB from "./assets/tb-placeholder.png";
+import 'fontsource-roboto';
+
+const SongTitle = "No Song Selected"
+const ArtistTitle = "No Artist"
+
 class App extends Component {
   render() {
     return (
       <div className= "App">
         <header className="App-header">
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
           <h1>React Music Visualizer</h1>
-          <img src={MusicNote} alt="Spotify Button" />
+          <img src={MusicNote} alt="Spotify Note" />
         </header>
+        <div className="Sub-header">
+          <h1 className="Song-title">{SongTitle}</h1>
+          <h2>{ArtistTitle}</h2>
+        </div>
+        <div className= "Main-panel">
+          <div className= "Button-column">
+            <Button variant="contained" color="primary">
+              Spheres
+            </Button>
+            <Button variant="contained" color="primary">
+              Bars
+            </Button>
+            <Button variant="contained" color="primary">
+              Equalizer
+            </Button>
+          </div>
+          <div className="Visualizer">
+            <img src={VisualizerPlaceholder} alt="Visualizer"/>
+          </div>
+          <div className="Equalizer">
+            <img src={TB} alt="Treble"/>
+          </div>
+        </div>
       </div>
     );
   }
