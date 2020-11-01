b6130a93cf4278ea9d6e526eec48862bc4e92bd9
AmyJPearl <amyperelberg@gmail.com>
2020-10-31 20:32:39 -0400

    Changes favicon adds title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { Component } from 'react';
 
+import MusicNote from "./assets/music-note.png"
 class App extends Component {
   render() {
     return (
       <div className= "App">
         <header className="App-header">
           <h1>React Music Visualizer</h1>
+          <img src={MusicNote} alt="Spotify Button" />
         </header>
       </div>
     );
